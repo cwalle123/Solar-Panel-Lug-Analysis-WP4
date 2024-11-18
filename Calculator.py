e470e7582958351217e83067238a8e8cce143f30
Julek-AK <188062174+Julek-AK@users.noreply.github.com>
2024-11-18 15:54:22 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,13 @@
+"""
+calculator.py verifies the validity of a single design, i.e. a single set of design parameters
+it runs instances of every check function and compiles the resulting safety margins to either
+print them out or pass them on to the iterator.py
+it also computes the mass of the design
+"""
+
+# External imports
+import numpy as np
+
+# Internal imports
+from checks import *
 
