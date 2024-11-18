b6ae58154781aa647facfbe6f65a8dd7e4501fe9
Julek-AK <188062174+Julek-AK@users.noreply.github.com>
2024-11-18 16:46:13 +0100

    Update Checks.py

--- Checks.py
+++ Checks.py
@@ -8,6 +8,12 @@ by calculator.py to verify an overall design
 import numpy as np
 
 
+# elastic modulus [Pa], yield strength [Pa], shear strength [Pa], density [kg/m^3], thermal expansion coefficient [1/K]
+materials = {
+    'Al 7075 T6': (71.7e9, 430e6, .5*430e6, 2810, 2.36e-5)
+}
+
+
 def vectorized(func):
     """decorator used to easily vectorise any function"""
     return np.vectorize(func)
