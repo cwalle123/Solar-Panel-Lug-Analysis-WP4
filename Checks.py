54f791f710747ce475b42b28096ffb3563b8ef04
Julek-AK <188062174+Julek-AK@users.noreply.github.com>
2024-11-18 15:54:03 +0100

    Update Checks.py

--- Checks.py
+++ Checks.py
@@ -1,1 +1,14 @@
+"""
+checks.py contains verification of all different failure modes
+each failure mode is given its own unique function that will be used
+by calculator.py to verify an overall design
+"""
+
+# External imports
+import numpy as np
+
+
+def vectorized(func):
+    """decorator used to easily vectorise any function"""
+    return np.vectorize(func)
 
