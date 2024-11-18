1e05d0cf0146cb89e687d7c81e346bfc472e54ce
cwalle123 <wcliftonjohn@gmail.com>
2024-11-18 17:08:16 +0100

    Update Checks.py

--- Checks.py
+++ Checks.py
@@ -8,7 +8,7 @@ by calculator.py to verify an overall design
 import numpy as np
 import math
 
-# elastic modulus [Pa], yield strength [Pa], shear strength [Pa], density [kg/m^3], thermal expansion coefficient [1/K]
+# Elastic modulus [Pa], yield strength [Pa], shear strength [Pa], density [kg/m^3], thermal expansion coefficient [1/K]
 materials = {
     'Al 7075 T6': (71.7e9, 430e6, .5*430e6, 2810, 2.36e-5)
 }
@@ -17,9 +17,9 @@ def vectorized(func):
     """decorator used to easily vectorise any function"""
     return np.vectorize(func)
 
-#Objective function
+# Objective function
 @vectorized
-def mass(Density_Lug, Thickness_Lug_1, Thickness_Lug_2, w_Lug, h_Lug, Diameter_Lug_1, Diameter_Lug_2): #all inputs should be given in SI units
+def mass(Density_Lug, Thickness_Lug_1, Thickness_Lug_2, w_Lug, h_Lug, Diameter_Lug_1, Diameter_Lug_2): #All inputs should be given in SI units
     """This function calculates the total mass of the lug by calculating and summing the volumes of
     the hinges and the backplate, and then multiplies the total volume by the density of the material"""
     HingeArea_Lug = w_Lug**(2) + (w_Lug**(2) * math.pi) / (8) - (Diameter_Lug_1**(2) * math.pi) / (4)  # m^2
