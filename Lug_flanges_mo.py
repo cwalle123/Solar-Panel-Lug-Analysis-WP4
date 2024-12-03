10c4cabd3b6d529fe4a3ec143d55847afcadd147
Mohameed <122169913+Aodaodaodv@users.noreply.github.com>
2024-12-03 11:44:02 +0100

    Update Lug_flanges_mo.py

--- Lug_flanges_mo.py
+++ Lug_flanges_mo.py
@@ -1,4 +1,11 @@
-#flanges design
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 10:20:29 2024
+
+@author: mkaha
+"""
+
+# flanges design
 import numpy as np
 # first order itration code in utf-8- bits
 #check the flanges thickness under axial-load in the (y-z) plane
@@ -12,14 +19,14 @@ fz = 1000
 Mx = 1000
 My = 1000
 Mz = 1000
-w1 = 30e-2 # base width assumed
+w = 30e-2 # base width assumed
 D1 = 10e-2 # hole diameter assumed
-W1D1 = w1/D1
+W1D1 = w/D1
 h = 40e-2  # flange profile height assumed 
 e = 15e-3  # location of the hole also was assumed 
-
+sigma_yield = 430e6
   
-def t_min_flage_axial_eq_6(w,D_1,sigma_yield,fy):
+def t_min_flage_axial_eq_6(w,D1,sigma_yield,fy):
     k1 = 0.5
     #f_axila = np.sqrt(fy**2+fz**2)
     #sigma_yield = 430e6
@@ -27,28 +34,39 @@ def t_min_flage_axial_eq_6(w,D_1,sigma_yield,fy):
     #D_1 = 15 #np.arange(1,1000,1 )
     #tot_param = f_axila/sigma_yield
     #find the smallest value for w ,D_1,t that is equal to tot_param 
-    t = fy/(sigma_yield*(w-D_1)*k1) 
+    t = fy/(sigma_yield*(w-D1)*k1) 
     return t
 
-t_min1 = t_min_flage_axial_eq_6(w1, D1, 430e6 , fy)*1000 # in mm
+t_min1 = t_min_flage_axial_eq_6(w,D1,sigma_yield , fy)*1000 # in mm
 #checking the tension-shear
 
 
-def t_min_flange_axial_shear(w,D_1,sigma_yield,fy):
+def t_min_flange_axial_shear(w,D1,sigma_yield,fy):
     k2 = 0.1
     t = fy/(sigma_yield*0.01*k2) 
     return t
-t_min2 = t_min_flange_axial_shear(w1, D1, 430e6 , fy)*1000 # in mm
+
+t_min2 = t_min_flange_axial_shear(w, D1, sigma_yield , fy)*1000 # in mm
+
+def Aav_over_Abearin_calculator(D1,h,e,w):
+    c = 6/((4*D1/(h-e))+((2*D1)/(2*np.pi*0.5*w)))
+    return c 
+
 
 def t_min_flage_transverse(w,D_1,sigma_yield,fy):
     k3 = 0.25
     t = fy/(sigma_yield*0.01*k3) 
     return t
 
-t_min3 = t_min_flage_transverse(w1, D1, 430e6, fy)*1000 # in mm
+t_min3 = t_min_flage_transverse(w, D1, sigma_yield , fy)*1000 # in mm
 
 #bearing check
 def bearing_check(sigma_yield,D1,t):
     F = sigma_yield * D1 * t
     return F
-f = bearing_check(430e6, D1, t_min2)/1000
+f = bearing_check(sigma_yield , D1, t_min2)/1000
+
+    
+    
+    
+         
