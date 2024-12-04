ecedf4e583ad450ddbb57b3afb53394a46c7a51c
Julek-AK <188062174+Julek-AK@users.noreply.github.com>
2024-12-04 12:58:56 +0100

    Update Checks.py

--- Checks.py
+++ Checks.py
@@ -8,27 +8,53 @@ by calculator.py to verify an overall design
 import numpy as np
 import math
 
-
 # elastic modulus [Pa], yield strength [Pa], shear strength [Pa], density [kg/m^3], thermal expansion coefficient [1/K]
 material_properties = {
-    'Al 7075 T6': (71.7e9, 430e6, .5*430e6, 2810, 2.36e-5),
-    'Ti-6Al-4V': (113.8e9, 880e6, .5*880e6, 4430, 8.6e-6),
-    '18-8 Stainless Steel': (193e9, 205e6, .5*205e6, 7900, 16.5e-6)
+    'Al 7075 T6': (71.7e9, 430e6, .5 * 430e6, 2810, 2.36e-5),
+    'Ti-6Al-4V': (113.8e9, 880e6, .5 * 880e6, 4430, 8.6e-6),
+    '18-8 Stainless Steel': (193e9, 205e6, .5 * 205e6, 7900, 16.5e-6)
 }
 
 # head diameter [m], shank diameter [m], shank length [m], d_sm [m], pitch [m], fastener area [m^2], stiffness area [m^2], tensile strength [Pa], thermal expansion coefficient [1/K]
 fastener_properties = {
     'M6x1': (0.01, 0.006, 0.01, 0.0048, 0.001, 2.83e-5, 1.79e-5, 8.96e8, material_properties['Ti-6Al-4V'][-1]),
-    'M0.5x0.125': (0.00094, 0.0005, 0.0015, 0.000347, 0.000125, 1.96e-7, 9.44e-8, 4.83e8, material_properties['18-8 Stainless Steel'][-1])
+    'M0.5x0.125': (0.00094, 0.0005, 0.0015, 0.000347, 0.000125, 1.96e-7, 9.44e-8, 4.83e8,
+                   material_properties['18-8 Stainless Steel'][-1]),
+    'M1x0.25': (
+    0.002, 0.001, 0.0015, 0.000693, 0.00025, 7.85e-7, 3.77e-7, 4.83e8, material_properties['18-8 Stainless Steel'][-1])
 }
 
 # minimum operating, maximum operating, reference (manufacturing) [K]
 temperatures = (250, 300, 288)
 
+# TODO SC wall thickness
+t_3 = .01  # [m]
+
+
+# Objective function
+def mass(Density_Lug, Thickness_Lug_1, Thickness_Lug_2, w_Lug, TotalLength_Lug, Diameter_Lug_1,
+         Diameter_Lug_2):  #all inputs should be given in SI units
+    """This function calculates the total mass of the lug by calculating and summing the volumes of
+    the hinges and the backplate, and then multiplies the total volume by the density of the material"""
+    HingeArea_Lug = w_Lug ** (2) + (w_Lug ** (2) * math.pi) / (8) - (Diameter_Lug_1 ** (2) * math.pi) / (4)  # m^2
+    TotalHingeVolume_Lug = 2 * Thickness_Lug_1 * HingeArea_Lug  # m^3
+    BackplateArea_Lug = TotalLength_Lug * w_Lug - Diameter_Lug_2 ** (2) * math.pi  # m^2
+    BackplateVolume_Lug = Thickness_Lug_2 * BackplateArea_Lug  # m^3
+    TotalVolume = TotalHingeVolume_Lug + BackplateVolume_Lug  # m^3
+    return Density_Lug * TotalVolume  # kg
+
 
 def positivity_check(design_vector):
     """Verifies no length is given as negative, as that would be nonsensical"""
     if np.all(design_vector > 0):
+        return True
+    else:
+        return False
+
+
+def beam_fit_check(h):
+    beam_diameter = 0.03
+    if h >= beam_diameter:
         return True
     else:
         return False
@@ -42,9 +68,10 @@ def thread_size_check(D_1, D_2, fastener):
     else:
         return False
 
+
 def X_compliance_check(t_1, X, e_2, h):
     """Verifies the horizontal direction passes thread clearance"""
-    if h + 2*t_1 + 4*e_2 <= X:
+    if h + 2 * t_1 + 4 * e_2 <= X:
         return True
     else:
         return False
@@ -52,14 +79,15 @@ def X_compliance_check(t_1, X, e_2, h):
 
 def w_compliance_check(w, D_1, D_2, e_1):
     """Verifies the vertical direction passes thread clearance"""
-    if 2*e_1 + 2*D_2 <= w and 3*D_1 <= w:
+    if 2 * e_1 + 2 * D_2 <= w and 2 * D_1 <= w:
         return True
     else:
         return False
 
 
 def thread_edge_distance_check(e_1, e_2, D_2):
-    if e_1 >= 1.5*D_2 and e_2 >= 1.5*D_2:
+    """Verifies the thread holes have sufficient clearance from lug edges"""
+    if e_1 >= 1.5 * D_2 and e_2 >= 1.5 * D_2:
         return True
     else:
         return False
@@ -104,10 +132,10 @@ def bearing_check(candidate_vector, force_vector, material):
 
     # Compute angles for fasteners
     angles = np.arctan2(fasteners[:, 1], fasteners[:, 0])
-    if My >= 0 :
-        angles_forces = angles + np.pi/2
-    else :
-        angles_forces = angles - np.pi/2
+    if My >= 0:
+        angles_forces = angles + np.pi / 2
+    else:
+        angles_forces = angles - np.pi / 2
 
     # Compute force components
     force_components = np.vstack([
@@ -128,9 +156,8 @@ def bearing_check(candidate_vector, force_vector, material):
     MS = (sigma_bearing / sigma_bearable) - 1
 
     # Compute the bearing of the wall
-    t_wall = 0.0005
     sigma_yield_wall = 345e6
-    sigma_wall = P_max / (t_wall * D_2)
+    sigma_wall = P_max / (t_3 * D_2)
     MS_wall = (sigma_yield_wall / sigma_wall) - 1
 
     return MS, MS_wall
@@ -147,10 +174,10 @@ def thermal_load_fastener(thermalExp_coef_fastener, thermalExp_coef_clamped, E, 
     max_temp = temp_working_max - temp_ref
 
     min_thermal_load = (thermalExp_coef_fastener - thermalExp_coef_clamped) * min_temp * E * A_stiffness * (
-                1 - force_ratio)
+            1 - force_ratio)
 
     max_thermal_load = (thermalExp_coef_fastener - thermalExp_coef_clamped) * max_temp * E * A_stiffness * (
-                1 - force_ratio)
+            1 - force_ratio)
 
     safety_factor = stress_max_fastener / (max([min_thermal_load, max_thermal_load]) / area_fastener) - 1
 
@@ -160,7 +187,6 @@ def thermal_load_fastener(thermalExp_coef_fastener, thermalExp_coef_clamped, E, 
 
 
 def pull_through_check(applied_load_vector, candidate_vector, material, fastener):
-
     # Material properties
     yield_stress = material_properties[material][1]  # Shear yield strength [Pa]
     D_fo = fastener_properties[fastener][0]  # Outer fastener head diameter [m]
@@ -190,9 +216,6 @@ def pull_through_check(applied_load_vector, candidate_vector, material, fastener
         [(x / 2 - e_2), (w / 2 - e_1)]
     ])
 
-    # Fixed wall thickness
-    t_3 = 0.0005  # Wall thickness [m]
-
     # Compute geometric properties
     dist = np.sqrt((x / 2 - e_2) ** 2 + (w / 2 - e_1) ** 2)  # Distance to fasteners [m]
     A_hole = 0.25 * np.pi * D_2 ** 2  # Area of a fastener hole [m²]
@@ -202,7 +225,8 @@ def pull_through_check(applied_load_vector, candidate_vector, material, fastener
     # Compute forces on fasteners
     M_z_total = - F_x * w + M_z
     M_x_total = F_z * w + M_x
-    F_in_plane_y = F_y / nf - M_x_total/((w - 2*e_1)*2) + M_z_total/((x-2*e_2)*2) # In-plane force due to F_y [N]
+    F_in_plane_y = F_y / nf - M_x_total / ((w - 2 * e_1) * 2) + M_z_total / (
+                (x - 2 * e_2) * 2)  # In-plane force due to F_y [N]
 
     # Compute stresses
     sigma_normal = F_in_plane_y / A_normal  # Normal stress due to F_y [Pa]
@@ -219,13 +243,75 @@ def pull_through_check(applied_load_vector, candidate_vector, material, fastener
 
     return MS_t_2, MS_t_3
 
-# Objective function
-def mass(Density_Lug, Thickness_Lug_1, Thickness_Lug_2, w_Lug, TotalLength_Lug, Diameter_Lug_1, Diameter_Lug_2): #all inputs should be given in SI units
-    """This function calculates the total mass of the lug by calculating and summing the volumes of
-    the hinges and the backplate, and then multiplies the total volume by the density of the material"""
-    HingeArea_Lug = w_Lug**(2) + (w_Lug**(2) * math.pi) / (8) - (Diameter_Lug_1**(2) * math.pi) / (4)  # m^2
-    TotalHingeVolume_Lug = 2 * Thickness_Lug_1 * HingeArea_Lug  # m^3
-    BackplateArea_Lug = TotalLength_Lug * w_Lug - Diameter_Lug_2**(2) * math.pi  # m^2
-    BackplateVolume_Lug = Thickness_Lug_2 * BackplateArea_Lug  # m^3
-    TotalVolume = TotalHingeVolume_Lug + BackplateVolume_Lug  # m^3
-    return Density_Lug * TotalVolume  # kg
+
+def flange_check(applied_load_vector, candidate_vector, material, fastener):
+    # Material properties
+    yield_stress = material_properties[material][1]  # Shear yield strength [Pa]
+
+    # Extract applied loads
+    F_x = applied_load_vector[0]  # Force in x-direction [N]
+    F_y = applied_load_vector[1]  # Force in y-direction [N]
+    F_z = applied_load_vector[2]  # Force in z-direction [N]
+
+    # Extract candidate parameters
+    D_1 = candidate_vector[3]  # Hole diameter [m]
+    t_1 = candidate_vector[0]  # Flange thickness [m]
+    w = candidate_vector[6]  # Flange height [m]
+    e = w / 2
+
+    # Find the relevant areas
+    def compute_Aav(w, D, e, t):
+        A1 = (w/2 - D/2 * np.cos(np.pi/4)) * t
+        A4 = A1
+        A2 = (w/2 - D/2) * t
+        A3 = (e - D/2) * t
+        Aav = 6 / (3/A1 + 1/A2 + 1/A3 + 1/A4)
+        return Aav
+
+    A_t = (w - D_1) * t_1  # Axial area
+    A_br = D_1 * t_1  # Bearing area
+    A_av = compute_Aav(w, D_1, e, t_1)
+
+    #  Pre-defined fitting functions obtained by sampling points from the pictures in the reader
+    def compute_Ktu(area_ratio):
+        if area_ratio < 0:
+            print("K_tu computation outside of valid range")
+            return np.NaN
+        elif area_ratio > 1.4:
+            area_ratio = 1.4
+        return 1.642 * area_ratio - 1.307 * area_ratio**2 + 0.342 * area_ratio**3
+
+    def compute_Kbru(eD):
+        if eD < 0.5:
+            print("K_bru computation outside of valid range")
+            return np.NaN
+        elif eD > 2.25:
+            return 1.7
+        return -1.443 + 3.647 * eD - 1.648 * eD**2 + 0.277 * eD**3
+
+    def compute_Kt(wD):
+        if wD < 1 or wD > 5:
+            print("K_t computation outside of valid range")
+            return np.NaN
+        return 1.244 - 0.351 * wD + 0.127 * wD**2 - 0.015 * wD**3
+
+    K_tu = compute_Ktu(A_av/A_br)
+    K_bru = compute_Kbru(e/D_1)
+    K_t = compute_Kt(w/D_1)
+
+    # Find the relevant margins
+    P_tu = K_tu * A_br * yield_stress
+    P_bru = K_bru * yield_stress * A_br
+    P_u = K_t * yield_stress * A_t
+
+    R_a = F_y / min(P_u, P_bru)
+    R_tr = F_z / P_tu
+
+    # Compute the safety margin
+    deviation = np.power(R_a, 1.6) + np.power(R_tr, 1.6)
+    SM = 1/np.power(deviation, 0.625) - 1
+
+    return SM
+
+
+
